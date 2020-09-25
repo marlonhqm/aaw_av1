8b66018d665d68df83b5542e08b92a9b39d5d0aa
Marlon Henrique Queroga Marques <mhqmarques@gmail.com>
2020-09-24 22:06:38 -0300

    iniciando projeto aaw_av1

---
+++ src/main/java/br/newtonpaiva/livro/LivroApplicattonApplication.java
@@ -0,0 +1,13 @@
+package br.newtonpaiva.livro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivroApplicattonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivroApplicattonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/newtonpaiva/livro/LivroApplicattonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.newtonpaiva.livro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivroApplicattonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
